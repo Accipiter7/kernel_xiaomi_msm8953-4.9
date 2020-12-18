39cbf854ad82801c43d718b8f9b6ce716a369bf8
Accip <64995687+Accipiter7@users.noreply.github.com>
2020-12-18 12:40:49 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 -j8 https://github.com/Accipiter7/AnyKernel3 -b mido /root/project/ak3
+
+# Set Build Env
+
+IMG=/root/project/c/out/arch/arm64/boot/Image.gz-dtb
+
+TC_VER=$("/root/project/pclang/bin/clang" --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+
+BDT=$(date +"%h-%d-%H:%M")
+
+export ARCH=arm64
+
+export CLANG_PATH=/root/project/pclang/bin
+
+export PATH=${CLANG_PATH}:${PATH}
+
+function sendInfo() 
+
+{
+
+ curl -s -X POST https://api.telegram.org/bot"$BID"/sendMessage -d chat_id="$GID" -d "parse_mode=HTML" -d text="$(
+
+  for POST in "${@}"; do
+
+   echo "${POST}"
+
+    done
+
+     )" 
+
+}
+
+function sendLog() 
+
+{
+
+ curl -F chat_id="${GID}" -F document=@/build.log https://api.telegram.org/bot"$BID"/sendDocument
+
+}
+
+function sendZip()
+
+{
+
+ cd /root/project/ak3 || exit
+
+ ZIP=$(echo *.zip)
+
+ curl -F chat_id="${GID}" -F document="@$ZIP"  https://api.telegram.org/bot"${BID}"/sendDocument
+
+}
+
+function zipper()
+
+{
+
+ cp "${IMG}" /root/project/ak3
+
+ cd /root/project/ak3 || exit 
+
+ make -j16
+
+ mv Thy-Kernel.zip Thy-"${BDT}".zip
+
+}
+
+function success() 
+
+{
+
+ sendInfo "<b>Commit: </b><code>$(git --no-pager log --pretty=format:'"%h - %s (%an)"' -1)</code>" \
+
+          "<b>Compile Time: </b><code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)</code>" \
+
+          "<b>Toolchain:</b><code>${TC_VER}</code>" \
+
+          "<b>THY SUCCESS</b>"
+
+ sendLog
+
+}
+
+function failed() 
+
+{
+
+ sendInfo "<b>Commit:</b><code>$(git --no-pager log --pretty=format:'"%h - %s (%an)"' -1)</code>" \
+
+          "<b> FAILED </b>" \
+
+          "<b>Total Time Elapsed: </b><code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds.</code>"
+
+ sendLog
+
+ exit 1;
+
+ }
+
+function compile() 
+
+{
+
+		cd /root/project/c || exit	    START=$(date +"%s")
+
+	    make ARCH=arm64 mido_defconfig O=out 2> /build.log
+
+	    PATH="/root/project/pclang/bin/:${PATH}" \
+
+		make O=out -j16 2>> /build.log \
+
+			CC=clang \
+
+			CROSS_COMPILE=aarch64-linux-gnu- \
+
+			CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+		
+
+if ! [ -a "$IMG" ] ; 
+
+then																		
+
+   END=$(date +"%s")
+
+   DIFF=$((END - START))
+
+   failed  
+
+fi
+
+  END=$(date +"%s")
+
+  DIFF=$((END - START))
+
+  success
+
+  zipper
+
+  sendZip
+
+}
+
+compile
